dc558dccfd2d664f496976373138b1ef3cff84b6
Konab <konab-job@yandex.ru>
2018-11-22 01:55:18 +0300

    add new config

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 
 class Config(object):
-	SECRET_KEY = os.environ.get('APP_SECRET_KEY') or ''
+	SECRET_KEY = os.environ.get('SECRET_KEY') or ''
 	SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or ''
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
+	# MAIL_SERVER = os.environ.get('MAIL_SERVER')
+	# MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
