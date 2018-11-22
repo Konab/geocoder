41af692d9985780971083c4569734c61ce665392
Konab <konab-job@yandex.ru>
2018-11-22 05:14:37 +0300

    Change models

--- app/models.py
+++ app/models.py
@@ -2,5 +2,5 @@ from app import db
 
 
 class Authors(db.Model):
-	id = db.Column(db.Ineger, primary_key=True)
+	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(120), index=True)
