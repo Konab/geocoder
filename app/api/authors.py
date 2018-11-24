972e1dcd027fa0745483ef5852323befa7b08af4
Konab <konab-job@yandex.ru>
2018-11-24 16:22:06 +0300

    first api commit

---
+++ app/api/authors.py
@@ -0,0 +1,19 @@
+from flask import jsonify
+from app.api import bp
+from app.models import Authors
+
+@bp.route('/authors/<init:id>', methods=['GET'])
+def get_author(id):
+	return jsonify(Authors.query.get_or404(id).to_dict())
+
+@bp.route('/authors', methods=['GET'])
+def get_authors():
+	pass
+
+@bp.route('/authors', methods=['POST'])
+def create_author():
+	pass
+
+@bp.route('/authors/<init:id>', methods=['PUT'])
+def update_author(id):
+	pass
--- app/models.py
+++ app/models.py
@@ -1,6 +1,49 @@
 from app import db
+from flask import url_for
 
 
-class Authors(db.Model):
+class PaginatedAPIMixin(object):
+	@staticmethod
+	def to_collection_dict(query, page, per_page, endpoint, **kwargs):
+		resources = query.paginate(page, per_page, False)
+		data = {
+			'items': [item.to_dict() for item in resources.items],
+			'_meta': {
+				'page': page,
+				'per_page': per_page,
+				'total_pages': resources.pages,
+				'total_items': resources.total
+			},
+			'_links': {
+				'self': url_for(endpoint, page=page, per_page=per_page,
+								**kwargs),
+				'next': url_for(endpoint, page=page + 1, per_page=per_page,
+								**kwargs) if resources.has_next else None,
+				'prev': url_for(endpoint, page=page - 1, per_page=per_page,
+								**kwargs) if resources.has_prev else None
+			}
+		}
+		return data
+
+
+class Authors(PaginatedAPIMixin, db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(120), index=True)
+
+	def to_dict(self, include_email=False):
+		data = {
+			'id': self.id,
+			'name': self.name,
+			'_links': {
+				'self': url_for('api.get_user', id=self.id)
+			}
+		}
+		if include_email:
+			data['email'] = self.email
+		return data
+
+	def from_dict(self, data, new_user=False):
+		for field in ['name']:
+			if field in data:
+				setattr(self, field, data[field])
+
