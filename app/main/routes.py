823caa29305a513728629d7e1fb973d0591bdbc7
Konab <konab-job@yandex.ru>
2018-12-01 01:15:40 +0300

    Test func postgres

--- app/main/routes.py
+++ app/main/routes.py
@@ -6,5 +6,5 @@ from sqlalchemy import func
 @bp.route("/")
 def index():
 	# name = Authors.query.filter_by(id=1).first().name
-	name = db.session.scalar(func(Cos_getaddrespoint('Оренбург Спартаковская 63')))
+	name = db.session.scalar(func.Cos_getaddrespoint('Оренбург Спартаковская 63'))
 	return "Hello, {}".format(name)
