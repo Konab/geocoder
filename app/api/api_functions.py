fbfb90a635437de77552711de1552366b5bcc183
Konab <konab-job@yandex.ru>
2018-12-14 23:25:23 +0300

    Test closest point functions for API

--- app/api/api_functions.py
+++ app/api/api_functions.py
@@ -2,4 +2,4 @@ from app import db
 from sqlalchemy import func
 
 def get_address_geom(address):
-	return response = db.session.scalar(func.Cos_getaddressgeom(address))
+	return db.session.scalar(func.Cos_getaddressgeom(address))
