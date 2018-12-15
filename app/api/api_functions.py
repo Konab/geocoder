dad7c230b3c522566d835c9d6d182e5dd6031375
Konab <konab-job@yandex.ru>
2018-12-15 12:52:10 +0300

    Test closest point functions for API

--- app/api/api_functions.py
+++ app/api/api_functions.py
@@ -5,6 +5,6 @@ def get_address_geom(address):
 	# Возвращает Geometry
 	return db.session.scalar(func.Cos_getaddressgeom(address))
 
-def get_street_geom(street, building):
+def get_street_geom(building, street):
 	# Возвращает Geometry
 	return db.session.scalar(func.Cos_getnearstreet(building, street))
